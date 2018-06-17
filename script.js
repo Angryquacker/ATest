1946ec7fc6833ee38fa801dc09a697e55a2b2bd8
Travis <tjxhahn@gmail.com>
2018-06-16 19:45:10 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+var ngApp = angular.module("myApp", []);
+                                    
+ngApp.controller('myCon', function($scope) {
+    $scope.message = [
+        {
+            title: 'Yo',
+            text: 'cake',
+            date: '1976-02-13',
+            location: 'Moravian Hall Square',
+            time: '08:24'
+        }, 
+        {
+            title: 'Sup',
+            text: 'food',
+            date: '1976-02-14',
+            location: 'Nazareth Circle',
+            time: '06:54'
+        }
+    ];
+    
+    $scope.addM = () => {
+        let text = document.getElementById('text').value;
+        let title = document.getElementById('title').value;
+        let date = document.getElementById('date').value;
+        let loc = document.getElementById('loc').value;
+        let time = document.getElementById('time').value;
+        
+        $scope.message.push({title: title, text: text, date: date, location: loc, time: time});
+    };
+});
